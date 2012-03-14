0ad744993f09e2fc560e523ef0d5be9fb06429fb
clindsay107 <clindsay107@gmail.com>
2012-03-13 22:13:37 -0700

    fixed some things

--- BPBot.py
+++ BPBot.py
@@ -3,13 +3,13 @@ An IRC bot written in Python for #fit on Rizon.
 Helps with conversions and 1RM estimates, as well
 as a few other things.
 
-v0.5 3/5/12 - now with 100% more SSL!
+v0.5 3/13/12 - now with 100% more SSL!
 '''
 
-import ssl, socket, re
+import ssl, socket, re, random
 
 server = "irc.rizon.net"
-chan = '#bptest'
+chan = '#fit'
 port = 6697
 nick = "BPBot"
 
@@ -26,14 +26,33 @@ def hello():
 	ircsock.send("PRIVMSG " + chan + " : hello\n")
 
 def logoff():
-	ircsock.send("PRIVMSG " + chan + " :Leaving\n") #Send our final goodbyes to the channel
-	ircsock.send("NICK fefg24g4425") #disown our nick to something random
-	ircsock.close()
+	ircsock.send("PRIVMSG " + chan + " :I'm leaving\n") #Send our final goodbyes to the channel
+	randomNick = 'nwoihgwnv' + str(random.randint(0,100)) #disown our nick to something random
+	ircsock.send("NICK " + randomNick)
+	ircsock.close() 
+
+def input(listed):
+	lift = '' #if the values never change, tell the user they misused the syntax
+	weight = ''
+	typesOfLifts = ['deadlift', 'benchpress', 'ohp', 'squat']
+	for l in typesOfLifts:
+		if (listed[1] == l):
+			lift = listed[1]
+
+	if re.search(r'\d+[lk][bg]', listed[2]): #still can log mathematical functions, fix this
+		weight = listed[2]
+
+	if (lift == '' or weight == ''):
+		print listed[2]
+		ircsock.send("PRIVMSG " + chan + " :Incorrect format, use !log deadlift 225lb\n")
+	else:
+		ircsock.send("PRIVMSG " + chan + " :Logging " + weight + " for your new " + lift + " record\n")
+	
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((server, port))
 ircsock = ssl.wrap_socket(s)
-ircsock.send("USER " + nick + " " + nick + " " + nick + " :this is a test\n")
+ircsock.send("USER " + nick + " " + nick + " " + nick + " :BPBot\n")
 ircsock.send("NICK " + nick + "\n")
 
 joinChan(chan)
@@ -53,9 +72,13 @@ while 1:
 		logoff()
 
 	if line.find(":!log") != -1:
-		messageToLog = re.compile('\!log\s.+', re.IGNORECASE)
-		string = messageToLog.search(line).group(1)
-		print string
-
-#f = open('test.txt')
-#f.written
+		try:
+			logReg = re.compile('\!log\s.+')
+			line = logReg.search(line).group(0)
+			listed = re.split('\s', line)
+			input(listed)
+		except AttributeError:
+			ircsock.send("PRIVMSG " + chan + " :You need to supply arguments!\n")
+		except IndexError:
+			ircsock.send("PRIVMSG " + chan + " :You need to supply ALL arguments!\n")
+			
