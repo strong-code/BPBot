5c063d2839283c3df3eb41b9b39e1f737ef60b46
clindsay107 <clindsay107@gmail.com>
2012-05-02 15:04:12 -0700

    successfully parses track title and artist, but need a way of handling usernames besides passing them in chat

--- modules/lastfm.py
+++ modules/lastfm.py
@@ -1,13 +1,17 @@
-
-from xml.dom import minidom
-import urllib2
+import re, urllib2
 
 _API_KEY_ = '7dffc948b8ce17eab6e88118e6c6e144'
-_URL_ = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=%s&api_key=%i'
+_URL_ = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=%s&api_key=%s'
 
 def getCurrentTrack(user):
-	#FIX THE STRING FORMATTING
-	url = _URL_ % user % _API_KEY_
-	dom = minidom.parse(urllib2.urlopen(url))
+	try:
+		url = _URL_ %(user, _API_KEY_)
+	except urllib2.HTTPerror:
+		return urllib2.HTTPerror
 
-	print dom.read()
+	xml = urllib2.urlopen(url).read()
+
+	artist = re.search('<artist.+\">(.+)<\/artist>', xml).group(1)
+	track = re.search('<name>(.+)<\/name>', xml).group(1)
+
+	return 'Currently listening to \'' + track.strip('\r\n') + '\' by ' + artist.strip('\r\n')
