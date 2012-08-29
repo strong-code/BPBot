99908b551ad60ac324eae252e94873788a6b6c3d
clindsay107 <clindsay107@gmail.com>
2012-08-29 01:29:20 -0700

    Fixed 1rm logging/retreiving functionality
    However this breaks on return1RM() for some reason... need to
    investigate

--- modules/database.py
+++ modules/database.py
@@ -2,45 +2,48 @@
 
 import sqlite3 as sql
 
+lifts = ['squat', 'dl', 'bench', 'ohp', 'c&j', 'snatch']
+
+#set up the database on start
 def setUp():
 	connection = sql.connect('BPBot.db')
 
 	with connection:
 		cur = connection.cursor()
-		cur.execute('CREATE TABLE IF NOT EXISTS RepMax(User TEXT PRIMARY KEY NOT NULL, Lift TEXT NOT NULL, Weight TEXT NOT NULL)')
-	connection.commit()
-	connection.close()
+		cur.execute('CREATE TABLE IF NOT EXISTS RepMax(User TEXT NOT NULL, Lift TEXT NOT NULL, Weight TEXT NOT NULL)')
+		cur.close()
+		connection.commit()
 	return True
 
+#Insert a 1RM lift into the database
 def insert1RM(user, lift, weight):
-	try:
-		connection = sql.connect('BPBot.db')
+	if lift not in lifts:
+		return 'Not a valid lift! Valid lifts are squat, dl, bench, ohp, c&j, snatch'
 
-		with connection:
-			cur = connection.cursor()
-			cur.execute('INSERT INTO RepMax(?, ?, ?)', user, lift, weight)
-			#cur.execute('UPDATE RepMax SET WEIGHT = (?) WHERE USER = (?) AND WHERE LIFT = (?)', weight, user, lift)
-			print '>>>'
+	data = [(user, lift, weight),]
+	connection = sql.connect('BPBot.db')
+
+	with connection:
+		cur = connection.cursor()
+		cur.execute('SELECT Lift FROM RepMax WHERE User = ?', [user])
+		rowExists = cur.fetchone()
+		if rowExists is None:
+			cur.executemany('INSERT INTO RepMax VALUES (?, ?, ?)', data)
+		else:
+			data = [(weight, user, lift),]
+			cur.executemany('UPDATE RepMax SET WEIGHT = ? WHERE USER = ? AND LIFT = ?', data)
+		cur.close()
 		connection.commit()
-		connection.close()
-		return True
+	return 'Successfully logged a new ' + lift + ' max of ' + weight + 'lb for ' + user
 
-	except:
-		return False
-
+#Return the 1RM weight of a specified lift for a specified user
 def return1RM(user, lift):
-	weight = ''
-	try:
+	if lift not in lifts:
+		return 'Not a valid lift! Valid lifts are squat, dl, bench, ohp, c&j, snatch'
+	else:
 		connection = sql.connect('BPBot.db')
-
 		with connection:
 			cur = connection.cursor()
-			cur.execute('SELECT Weight FROM RepMax WHERE User = (?) AND WHERE Lift = (?) LIMIT 1', user, lift)
-			for row in rows:
-				weight = str(row)
-		connection.commit()
-		connection.close()
-		return user + ' has a 1RM ' + lift + ' of ' + weight
-
-	except:
-		return False
+			for row in cur.execute('SELECT Weight FROM RepMax WHERE User = ? AND Lift = ?', (user, lift)):
+				return user + ' has a 1RM ' + lift + ' of ' + str(row[0]) + 'lb'
+			cur.close()
