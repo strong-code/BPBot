4373d953849b1f8065e3f5d6e22949db3005f4a9
clindsay107 <clindsay107@gmail.com>
2012-09-24 00:00:44 -0700

    Basica conversions for lb <-> kg
    Things to add? height/distance/fluid volume?

---
+++ modules/conversion.py
@@ -0,0 +1,20 @@
+def convert(weight):
+	if 'kg' in weight:
+		return kiloToPound(weight)
+	else: return poundToKilo(weight)
+
+def kiloToPound(weight):
+	weight = weight.replace('kg', '').strip()
+	try:
+		weight = float(weight) * 2.2
+		return 'Thats ' + ('%.2f' % round(weight, 2)) + 'lb'
+	except:
+		return
+
+def poundToKilo(weight):
+	weight = weight.replace('lb', '').strip()
+	try:
+		weight = float(weight) / 2.2
+		return 'Thats ' + ('%.2f' % round(weight, 2)) + 'kg'
+	except:
+		return
